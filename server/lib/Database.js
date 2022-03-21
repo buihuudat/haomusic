1603ed76be6a0ba3d1bda926d2f0a010ef7e51f0
Bùi Hữu Đạt <dat54261001@gmail.com>
2022-03-21 17:52:59 +0700

    hao han music first push

--- server/lib/Database.js
+++ server/lib/Database.js
@@ -5,7 +5,7 @@ exports.init = function () {
   return new Promise((resolve, reject) => {
     mongoose.Promise = global.Promise;
     const conn = mongoose.connection;
-    mongoose.connect('mongodb+srv://haohanmusic:haohanmusic123@cluster0.8wkov.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://haohanmusic:haohanmusic123@cluster0.8wkov.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
